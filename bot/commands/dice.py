9036f9f917f08cb7efcf477f92955255adea3c2d
Alexsey <84779107+razenxc@users.noreply.github.com>
2023-04-12 22:15:57 +0300

    Create dice.py

---
+++ bot/commands/dice.py
@@ -0,0 +1,24 @@
+import random
+import disnake
+from disnake.ext import commands
+
+class Dice(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.slash_command(description="Rolls a dice.")
+    @commands.cooldown(1, 15, commands.BucketType.user)
+    async def dice(self, ctx, sides: int = 6):
+        """Rolls a dice with the specified number of sides."""
+        if sides < 1:
+            await ctx.response.send_message("The dice must have at least 1 side.", ephemeral=True)
+        else:
+            result = random.randint(1, sides)
+            embed = disnake.Embed(
+                title=f"The dice rolled {result}!",
+                color=disnake.Color.green()
+            )
+            await ctx.response.send_message(embed=embed, ephemeral=False)
+
+def setup(bot):
+    bot.add_cog(Dice(bot))
